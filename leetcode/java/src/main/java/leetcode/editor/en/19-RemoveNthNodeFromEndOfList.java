2c52605181614b7236ccad83c4558b4ed2ec0560
TianyuanZhao <47087965+TianyuanZhao@users.noreply.github.com>
2025-03-18 02:27:45 -0700

    Add lubuladong solution

--- leetcode/java/src/main/java/leetcode/editor/en/19-RemoveNthNodeFromEndOfList.java
+++ leetcode/java/src/main/java/leetcode/editor/en/19-RemoveNthNodeFromEndOfList.java
@@ -104,3 +104,19 @@ class Solution {
 
  */
 
+//  class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         // 虚拟头结点
+//         ListNode dummy = new ListNode(-1);
+//         dummy.next = head;
+//         // 删除倒数第 n 个，要先找倒数第 n + 1 个节点
+//         ListNode x = findFromEnd(dummy, n + 1);
+//         // 删掉倒数第 n 个节点
+//         x.next = x.next.next;
+//         return dummy.next;
+//     }
+        
+//     private ListNode findFromEnd(ListNode head, int k) {
+//         // 代码见上文
+//     }
+// }
