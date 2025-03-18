f1a66cba1ea183bf8f81df0802e282fcb744d79e
Tianyuan Zhao <tianyuanzhao@Tianyuandebijibendiannao.local>
2025-03-17 22:04:27 -0700

    Set up the VS code workspace and the first codebase

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ A Record of My Problem Solving Journey.
 
 # About me
 Senior Software Engineer with 8+ years of experience building and optimizing large-scale distributed systems, backend services, and cloud-native applications. Proficient in designing highly available, scalable technical solutions with expertise in cloud security, threat detection, and ML-driven anomaly detection for e-commerce. Proven ability to solve complex problems, deliver high-impact projects, and drive cross-functional collaboration to advance business growth and engineering excellence.
+
+# LeetCode Plugin Code Template
+
+It allows you to debug LeetCode algorithm codes locally in **vscode** or **Jetbrains IDE**, currently supports **Java/C++/Python/Golang/JavaScript**.
+
+Detailed configuration methods are available in [Jetbrains LeetCode Plugin Usage Guide](https://labuladong.online/algo/en/intro/jetbrains/) and [vscode LeetCode Plugin Usage Guide](https://labuladong.online/algo/en/intro/vscode/).
+
+Welcome to submit Issue/PR to optimize this repository or add new language support.
+
+# Exmaples
+
+![image](https://github.com/user-attachments/assets/6906ce3f-afd3-4b77-a3cf-cd4ec013fb22)
+
+![image](https://github.com/user-attachments/assets/5864e01f-678b-4f6a-b1e1-3c22f30a2266)
---
+++ leetcode/java/src/main/java/leetcode/editor/en/21-MergeTwoSortedLists.java
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ * @lcpr version=30103
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (66.09%)
+ * Likes:    23033
+ * Dislikes: 2252
+ * Total Accepted:    5M
+ * Total Submissions: 7.6M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * 
+ * Merge the two lists into one sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ * 
+ * Return the head of the merged linked list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode mergedList = new ListNode(0);
+        ListNode head = mergedList;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                mergedList.next = list1;
+                list1 = list1.next;
+            } else {
+                mergedList.next = list2;
+                list2 = list2.next;
+            }
+            mergedList = mergedList.next;
+        }
+
+        if (list1 != null) {
+            mergedList.next = list1;
+        } else if (list2 != null) {
+            mergedList.next = list2;
+        }
+        
+        head = head.next;
+        return head;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[0]\n
+// @lcpr case=end
+
+ */
+
