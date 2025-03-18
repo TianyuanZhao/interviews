bbacb7e0a4d03e00ec68a6cabf20595632e53925
TianyuanZhao <47087965+TianyuanZhao@users.noreply.github.com>
2025-03-18 01:55:53 -0700

    Add 19-RemoveNthNodeFromEndOfList, templated solution file

---
+++ leetcode/java/src/main/java/leetcode/editor/en/19-RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=19 lang=java
+ * @lcpr version=30103
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (48.20%)
+ * Likes:    19729
+ * Dislikes: 845
+ * Total Accepted:    3.3M
+ * Total Submissions: 6.9M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given the head of a linked list, remove the n^th node from the end of the
+ * list and return its head.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: head = [1], n = 1
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * 
+ * 
+ * 
+ * Follow up: Could you do this in one pass?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n1\n
+// @lcpr case=end
+
+ */
+
