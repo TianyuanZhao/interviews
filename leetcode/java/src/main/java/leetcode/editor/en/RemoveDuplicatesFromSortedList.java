051aef5e136ecbbd1400a8ef2181fb5ec72e7505
Tianyuan Zhao <47087965+TianyuanZhao@users.noreply.github.com>
2025-03-23 22:11:34 -0700

    Add labuladong's solution for 83-RemoveDuplicatesFromSortedList

--- leetcode/java/src/main/java/leetcode/editor/en/RemoveDuplicatesFromSortedList.java
+++ leetcode/java/src/main/java/leetcode/editor/en/RemoveDuplicatesFromSortedList.java
@@ -98,3 +98,22 @@ public class RemoveDuplicatesFromSortedList {
 
  */
 
+//  class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if (head == null) return null;
+//         ListNode slow = head, fast = head;
+//         while (fast != null) {
+//             if (fast.val != slow.val) {
+//                 // nums[slow] = nums[fast];
+//                 slow.next = fast;
+//                 // slow++;
+//                 slow = slow.next;
+//             }
+//             // fast++
+//             fast = fast.next;
+//         }
+//         // 断开与后面重复元素的连接
+//         slow.next = null;
+//         return head;
+//     }
+// }
