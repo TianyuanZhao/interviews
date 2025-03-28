a7f3723c47a9eb09418a5c5d6456c1744402fcb3
Tianyuan Zhao <47087965+TianyuanZhao@users.noreply.github.com>
2025-03-27 21:52:48 -0700

    Add labuladong solution for 232-ImplementQueueUsingStacks

--- leetcode/java/src/main/java/leetcode/editor/en/ImplementQueueUsingStacks.java
+++ leetcode/java/src/main/java/leetcode/editor/en/ImplementQueueUsingStacks.java
@@ -138,3 +138,40 @@ public class ImplementQueueUsingStacks {
 
 
 
+// labuladong solution - better time complexity
+// class MyQueue {
+//     private Stack<Integer> s1, s2;
+    
+//     public MyQueue() {
+//         s1 = new Stack<>();
+//         s2 = new Stack<>();
+//     }
+
+//     // 添加元素到队尾
+//     public void push(int x) {
+//         s1.push(x);
+//     }
+    
+//     // 返回队头元素
+//     public int peek() {
+//         if (s2.isEmpty())
+//             // 把 s1 元素压入 s2
+//             while (!s1.isEmpty()) {
+//                 s2.push(s1.pop());
+//             }
+//         return s2.peek();
+//     }
+
+//     // 删除队头元素并返回
+//     public int pop() {
+//         // 先调用 peek 保证 s2 非空
+//         peek();
+//         return s2.pop();
+//     }
+
+//      // 判断队列是否为空
+//      // 两个栈都为空才说明队列为空
+//     public boolean empty() {
+//         return s1.isEmpty() && s2.isEmpty();
+//     }
+// }
